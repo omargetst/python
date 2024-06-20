0c39684d69a0e620074b0c76817becfb42afb24f
omargetst <lci.omar.gonzalez@gmail.com>
2024-06-19 23:26:28 -0600

    primer version sin funciones Primon.py

---
+++ Primo.py
@@ -0,0 +1,25 @@
+ # Escribe un programa que se encargue de comprobar si un número es o no primo.
+ # Hecho esto, imprime los números primos entre 1 y 100.
+
+
+try :
+    while True:
+        number = int(input ("Numero a verificar: "))
+        if number == 0: 
+            print ("El número debe de ser mayor a 0")
+        else:
+            break
+      
+    residuo = 0
+    if number == 1:
+        print ("El número {} es un número primo " .format(number))
+    else:
+        for i in range (2, number):
+            if (number%i)==0:
+                print ("El número {} es divisible entre {} por lo tanto NO es un número primo " .format(number, i))
+                break
+            else:
+                if i == (number-1): print ("El número {} es un número primo " .format(number))
+        
+except ValueError:
+    print ("Favor de insertar un número entero!!")
